c4da1dd5f51c3b1894c0468aea5474d71340b3c9
Speks2 <142408255+Speks2@users.noreply.github.com>
2023-11-20 08:43:17 +0100

    new
    dada

--- assets/js/if-then.js
+++ assets/js/if-then.js
@@ -8,33 +8,53 @@ const E = 40;
 
 // Opgave 1: skriv et expression der viser true når A er lig med B.
 
-if ( /*..dit expression Her*/ ) {
-    console.log('statement true!');
+if ( 'A' == 'B' ); {
+    console.log('statement true!');   
 }
 
 
 //opgave 2: skrive en If then statement der vurderer om A er større end B.
 
+if ( A > B && A < C); {
+    console.log('statement truee!');
+}
 
 
 
 //opgave 3: Skriv en If then statement der vurderer om A er mindre end eller lig med B
 
-
-
+if ( A <= B); {
+    console.log('statement truue!');
+}
+ 
 
 
 //opgave 4: Skriv en If then statement der bliver FALSE når du sammenligner B og C.
 
+let (B = 5);
+let (C = '5');
+
+if (B === C) {
+  console.log("B er lig med C");
+} else {
+  console.log("B er ikke lig med C");
+}
 
 
 
 
 //opgave 5: Skriv en If then statement der bliver TRUE når du sammenligner B og C.
+let (B = 5);
+let (C = 5);
 
-
-
+if (B == C) {
+  console.log("B er lig med C");
+} else {
+  console.log("B er ikke lig med C");
+}
 
 //opgave 6: Skriv en If then statement der sammenligner B = C og E = A hvis B=C skal den blive TRUE. 
-
+if (B==A || E==C) {
+    console.Log('Statement True!')
+}
 
--- assets/js/operators.js
+++ assets/js/operators.js
@@ -14,14 +14,34 @@ if ( /*..dit expression Her*/ ) {
 
 
 //opgave 2: skrive en If then statement der bliver TRUE når  A er større end B og A er mindre end E.
+let (A = 7);
+let (B = 5);
+let (E = 10);
 
-
-
+if (A > B && A < E) {
+  console.log("A er større end B OG mindre end E");
+} else {
+  console.log("A er enten ikke større end B eller ikke mindre end E");
+}
 
 //opgave 3: Skriv en If then statement med  && og  || der kun er sand når alle expressions er sande.
+let A = 7;
+let B = 5;
+let C = 10;
+let D = 2;
+let E = 12;
 
-
-
-
-
+if (A > B && A < E && B < C && D > B) {
+  console.log("Alle betingelser er sande");
+} else {
+  console.log("Mindst én betingelse er ikke sand");
+}
 //opgave 4: Skriv en If then statement der kan finde ud af om F er "undefined" eller har en værdi, ved at bruge !.
+
+let F;
+
+if (!F) {
+  console.log("Variablen F er undefined eller har ingen værdi");
+} else {
+  console.log("Variablen F har en værdi");
+}
